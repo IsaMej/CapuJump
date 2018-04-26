15e681247253df216bd6d0b69aa3031d75eae6f1
IsaMej <isabelle.mejias.martin@gmail.com>
2018-04-26 11:02:01 +0200

    ajout credits team

--- view/credit.js
+++ view/credit.js
@@ -5,14 +5,45 @@ export default class Credit extends Component {
     static navigationOptions = {
         title: 'Credit'
     }
-    render() {
-        const { navigate } = this.props.navigation;
-        return (
-            <View>
-                <View>
-                    <Text>Credit</Text>
-                </View>
-            </View>
-        );
-    }
+  render() {
+    const { navigate } = this.props.navigation;
+    let pic = {
+        uri: 'https://www.pokebip.com/pokedex-images/artworks/190.png'
+    };
+    return (
+      <View style={styles.bg}>
+        <Image source={pic} style={styles.image}/>
+        <Text style={styles.title}>CREDITS</Text>
+        <Text style={styles.textCredits}>*Team Capupu*</Text>
+        <Text>Rémi Béziat</Text>
+        <Text>Léa Gattesco</Text>
+        <Text>Nicolas Hamelin</Text>
+        <Text>Isabelle Mejias-Martin</Text>
+        <Text>Alexandre Ochman</Text>
+      </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create ({
+    bg: {
+        flex: 1,
+        backgroundColor: '#AB7ABC',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 50,
+        color: '#EEE29A',
+    },
+    textCredits: {
+        fontWeight: 'bold',
+        fontSize: 20,
+        color: '#FFFFFF',
+    },
+    image: {
+        width: 293, 
+        height: 210,
+    },
+});
